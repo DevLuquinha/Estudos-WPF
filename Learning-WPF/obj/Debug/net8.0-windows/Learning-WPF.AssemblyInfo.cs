574e05464fbf53e54b3f3bc0992dd4751f509de2
Lucas Dev <lucasemmanuelrodrigues737@gmail.com>
2025-07-22 10:56:37 -0300

    Creating the ViewModel directory, and add classes

--- Learning-WPF/obj/Debug/net8.0-windows/Learning-WPF.AssemblyInfo.cs
+++ Learning-WPF/obj/Debug/net8.0-windows/Learning-WPF.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Learning-WPF")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3898c0d6a6c8ba9df6ba3105e5429e683b64b521")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6ab172972209f346f1e44cf954da486ff1b86794")]
 [assembly: System.Reflection.AssemblyProductAttribute("Learning-WPF")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Learning-WPF")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
