589efcfe3e9753c0e9ecdaf9b05a116f4ca0256f
Lucas Dev <lucasemmanuelrodrigues737@gmail.com>
2025-07-28 14:32:08 -0300

    Data validation, using the INotifyDataErrorInfo

--- Learning-WPF/obj/Debug/net8.0-windows/Learning-WPF.AssemblyInfo.cs
+++ Learning-WPF/obj/Debug/net8.0-windows/Learning-WPF.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Learning-WPF")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d797ece2d576e8dd8bc457f06f5e3513e8bfb0d2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0a301552411eb2ecd4abcb6e22cf56d669e63657")]
 [assembly: System.Reflection.AssemblyProductAttribute("Learning-WPF")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Learning-WPF")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
