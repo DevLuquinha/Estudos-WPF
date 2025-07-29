d067c6241dcb6a7091ea3a299518bc77a5ccbe92
Lucas Dev <lucasemmanuelrodrigues737@gmail.com>
2025-07-29 09:40:40 -0300

    Create Client Class, by INotifyPropertyChange

---
+++ ComboBox-Study/Client.cs
@@ -0,0 +1,58 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace ComboBox_Study
+{
+    public class Client : INotifyPropertyChanged
+    {
+		private string _name;
+        private string _lastName;
+		private string _phone;
+		
+		public event PropertyChangedEventHandler? PropertyChanged;
+		
+		public string Name
+		{
+			get => _name;
+            set 
+			{ 
+				if(_name != value)
+				{
+                    _name = value;
+					OnPropertyChanged();
+                }
+			}
+		}
+
+        public string LastName
+        {
+			get => _lastName;
+            set 
+			{ 
+				if(_lastName != value)
+				{
+					_lastName = value; 
+					OnPropertyChanged();
+                }
+			}
+        }
+
+		public string Phone
+		{
+			get => _phone;
+			set 
+			{ 
+				if(_phone != value)
+				{
+                    _phone = value;
+                    OnPropertyChanged();
+                }
+            }
+		}
+
+		public void OnPropertyChanged([CallerMemberName] string propertyName = null)
+		{
+			PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
--- ComboBox-Study/obj/Debug/net9.0-windows/ComboBox-Study.AssemblyInfo.cs
+++ ComboBox-Study/obj/Debug/net9.0-windows/ComboBox-Study.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ComboBox-Study")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f7afa52322470080b6c1613b1fd23facfe054d04")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f2730455c410aab95bea8536d783f3b72e9e177b")]
 [assembly: System.Reflection.AssemblyProductAttribute("ComboBox-Study")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ComboBox-Study")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
